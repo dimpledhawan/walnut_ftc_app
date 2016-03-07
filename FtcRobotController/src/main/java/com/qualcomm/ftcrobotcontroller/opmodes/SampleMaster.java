79398d7b4e7a2d8a51ebafbf0643a61d6c77723d
yan vologzhanin <yanvolo@gmail.com>
2016-03-07 11:34:40 -0500

    Structured Sample Properly

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SampleMaster.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SampleMaster.java
@@ -7,11 +7,17 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
  */
 public class SampleMaster extends LinearOpMode{
     public void runOpMode(){
+        //Prepare
         telemetry.addData("Tests","Starting in Sample Master");
-        MasterLinear sample = new MasterLinear("RED",1,1000,false,this);
-        //Check for null pointers
-        //Init Stage
-
-        sample.runOpMode();
+        MasterLinear sample = new MasterLinear("RED",1,2,false,this);
+        telemetry.addData("Tests", "I'M READY FOR FREADY!");
+        //Do
+        try{
+            waitForStart();
+            sample.execute();
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
     }
 }
