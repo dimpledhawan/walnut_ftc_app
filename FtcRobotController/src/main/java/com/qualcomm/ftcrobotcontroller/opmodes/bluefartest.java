575cbc936d8c69d50eab5d8c55f1a20904d366e8
yan vologzhanin <yanvolo@gmail.com>
2016-03-15 14:14:15 -0400

    Create bluefartest.java

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/bluefartest.java
@@ -0,0 +1,25 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Walnut Robotics on 3/13/2016.
+ */
+public class bluefartest extends LinearOpMode
+{
+    public void runOpMode(){
+        //Prepare
+        telemetry.addData("Tests", "Starting in Sample Master");
+        AutoSections stepToTest = Autosections.POSITION2START;
+        TestLinear sample = new MasterLinear("BLUE",2,0,false,this);
+        telemetry.addData("Tests", "I'M READY FOR FREADY!");
+        //Do
+        try{
+            waitForStart();
+            sample.execute();
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+    }
+}
